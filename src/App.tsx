6cedf3ca10a0cee67bfddebac79f84d7d389ceee
Stanislav Kozin <stas@staskozin.ru>
2024-10-11 02:18:08 +0300

    Набросал основную верстку

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1>Список дел</h1>
+
+        <div>
+          <span>Показывать:</span>
+          <label>
+            <input type="radio" />
+            <span>Все</span>
+          </label>
+          <label>
+            <input type="radio" />
+            <span>Активные</span>
+          </label>
+          <label>
+            <input type="radio" />
+            <span>Выполненные</span>
+          </label>
+        </div>
+
+        <span>Выполнено 0 из 10 задач</span>
+
+        <button>Удалить выполненные</button>
+
+        <div className="add">
+          <input className="add__input" type="text" />
+          <button className="add__button"><img src="icon/plus.svg" alt="" /></button>
+        </div>
+
+        <ul>
+          <li>
+            <input type="checkbox" />
+            <textarea spellCheck="true"></textarea>
+            <button><img src="icon/trash.svg" alt="" /></button>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <textarea spellCheck="false"></textarea>
+            <button><img src="icon/trash.svg" alt="" /></button>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <textarea spellCheck="false"></textarea>
+            <button><img src="icon/trash.svg" alt="" /></button>
+          </li>
+        </ul>
+      </div>
+
+      <footer>Сделал <a href="https://staskozin.ru">Станислав Козин</a> в 2024 году</footer>
+    </>
   );
 }
 
